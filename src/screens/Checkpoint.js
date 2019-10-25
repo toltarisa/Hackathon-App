e5907d7597cf0536024cfcfa53baabc163364e24
Alertis <osmanyavuzdemir96@gmail.com>
2019-10-25 08:59:48 +0300

    Harita sayfası tamamlandı

--- src/screens/Checkpoint.js
+++ src/screens/Checkpoint.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import Maps from '../components/Maps'
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 class Checkpoint extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      mapOpen:false
+    };
+  }
+
+  openMap = () => {
+    this.setState({mapOpen:!this.state.mapOpen})
   }
 
   render() {
     return (
       <View style={styles.main}>
-        <Text>Checkpoint Page</Text>
+      {this.state.mapOpen ? <Maps openMap={this.openMap}/> : 
+        <View style={styles.buttonBox}>
+          <TouchableHighlight style={styles.myButton} onPress={this.openMap}>
+              <Text style={styles.buttonText}>En Yakın Toplanma Alanını Bul</Text>
+            </TouchableHighlight>
+        </View>
+        }
+       
       </View>
     );
   }
@@ -19,10 +34,32 @@ class Checkpoint extends Component {
 
 const styles = StyleSheet.create({
   main: {
+    flex: 1
+  },
+  buttonBox:{
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  myButton:{
+    padding: 5,
+    height: 200,
+    width: 200,  //The Width must be the same as the height
+    borderRadius:400, //Then Make the Border Radius twice the size of width or Height   
+    backgroundColor:'red',
+
+
+  },
+  buttonText:{
+    flex:1,
+    width:190,
+    textAlign:"center",
+    marginTop:40,
+    fontSize:28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+  }
 });
 
 export default Checkpoint;
